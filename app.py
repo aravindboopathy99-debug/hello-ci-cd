8cf8c3333ca33214a63aa70a1eb9f0c274f96a0a
aravindboopathy99-debug <aravindboopathy99@gmail.com>
2026-01-21 14:02:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
  
 @app.route("/") 
 def hello(): 
-    return "Hello World from CI/CD!" 
+    return "Hello World final version!" 
  
 if __name__ == "__main__": 
     app.run() 
